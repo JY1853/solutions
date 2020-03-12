5edae5d340e5cfb035ce85f57abc05875f978c44
Cheng Jun Yuan <supergambit1@gmail.com>
2020-03-12 22:51:16 +0800

    Update temp.py

--- solns/temp.py
+++ solns/temp.py
@@ -1,1 +1,13 @@
+ans = 2
+a = 1
+b = 2
 
+while b < 4000000:
+    a += b
+    if a % 2 == 0 and a < 4000000:
+        ans += a
+    b += a
+    if b % 2 == 0 and b < 4000000:
+        ans += b
+
+print(ans)

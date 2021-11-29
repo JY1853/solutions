b5fd66f5d32abbc609604290ca7fd529c244a729
Cheng Jun Yuan <supergambit1@gmail.com>
2021-11-29 10:53:32 +0800

    Create 011.py

---
+++ projecteuler/011.py
@@ -0,0 +1,3 @@
+# i solved this problem by inspection, will add python later.
+# since you only need to multiply 4 numbers, i scanned and saw the combo "87 97 94 89" (from bottom left to top right)
+# as it turns out, that is the answer.
